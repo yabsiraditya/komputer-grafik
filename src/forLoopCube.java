3086ac8597584a231c33f942216e08ec1ca725dd
yabsiraditya <yabsir.aditya@gmail.com>
2024-10-20 20:19:03 +0700

    update KompGraf

--- src/forLoopCube.java
+++ src/forLoopCube.java
@@ -10,14 +10,19 @@ import java.awt.Color;
 public class forLoopCube extends JApplet {
 
     public void paint(Graphics g) {
+    this.setSize(500, 200);
     Color c[] = {Color.green,Color.blue,Color.yellow,Color.red,Color.cyan}; 
     int x = 10;
     int y = 10;
+    int xPts[] = {5, 25, 50, 30, 15, 5};
+    int yPts[] = {10, 35, 20, 65, 40, 10};
         for (int i = 0; i < 2; i++) {
             x = 10;
             for (int j = 0; j < 5; j++) {
                 g.setColor(c[j]);
-                g.fillRoundRect(x, y, 60, 60,100,100);
+                g.drawPolygon(xPts, yPts, xPts.length);
+                g.drawRect(x, y, 60, 60);
+                g.fillRoundRect(x,y,60,60,100,100);
                 x += 70;   
             }
          y += 70;
